816d50df13d35b337d32dc77f64d51f40907fd82
Doyeon Kim <wii021004@naver.com>
2025-02-17 09:34:49 +0900

    백준 11047번 동전 0 (실버4/)

---
+++ BaekJoon11047.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class BaekJoon11047 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] coins = new int[n];
+        int answer = 0;
+
+        //오름차순으로 주어짐
+        for(int i=0; i<n; i++){
+            coins[i] = sc.nextInt();
+        }
+
+        //계산
+        for(int i=n-1; i>-1; i--){
+            if(k / coins[i] > 0){
+                answer += k / coins[i];
+                k = k % coins[i];
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}
